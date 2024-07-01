7e67a00d6bf0aa4ce93d5e3d86f41d812710c65a
Wellington Cerqueira Araujo <92543615+wcerqueiraA@users.noreply.github.com>
2024-07-01 14:54:46 -0300

    Update Editar.cshtml.cs

--- TarefaApp/Pages/TarefaMaster/Editar.cshtml.cs
+++ TarefaApp/Pages/TarefaMaster/Editar.cshtml.cs
@@ -34,8 +34,6 @@ namespace TarefaApp.Pages.TarefaMaster
             return Page();
         }
 
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
